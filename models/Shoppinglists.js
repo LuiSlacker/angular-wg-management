aaa87107ae275c8e01bb675f958228ed3e04a392
Petulant Slacker <l.goohsen@gmail.com>
2015-12-31 14:16:00 +0100

    added REST-Routes for Shoppinglists

--- models/Shoppinglists.js
+++ models/Shoppinglists.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.model('Shoppinglist', new mongoose.Schema({
     name: String,
-    date: {type: Date, default: new Date()},
+    date: {type: Date, default: Date.now},
     wg: {type: mongoose.Schema.Types.ObjectId, ref: 'WG'}
 }));
--- routes/allWGs.js
+++ routes/allWGs.js
@@ -5,15 +5,35 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var mongoose = require('mongoose');
 
-// MongoDB model  ==================================================
+// MongoDB models  =================================================
 var WG = mongoose.model('WG');
+var Shoppinglist = mongoose.model('Shoppinglist');
 
+// WG Routing Parameter ============================================
+router.param('wg', function(req, res, next, id){
+    WG.findById(id, function(err, wg){
+        if (err){
+            next(err)
+        } else if (!wg) {
+            next(new Error('Failed to load WG'));
+        } else{
+            req.wg = wg;
+            next();
+        }
+    })
+});
+
+// WG Routes  ======================================================
 router.get('/', function(req, res, next){
     WG.find(function(err, wgs){
         if (err) return next(err);
 
         res.json(wgs)
     });
+});
+
+router.get('/:wg', function(req, res){
+    res.json(req.wg);
 });
 
 router.post('/', jsonParser, function(req, res, next){
@@ -25,9 +45,54 @@ router.post('/', jsonParser, function(req, res, next){
 });
 
 router.delete('/:wg', function(req, res, next){
-    console.log(req.params.wg);
-    WG.remove({"_id": req.params.wg}, function(err){
+    WG.remove({"_id": req.wg}, function(err){
         if (err) return next(err);
+        res.sendStatus(204);
+    });
+});
+
+
+// Shoppinglist Routing Parameter  ===================================
+router.param('shoppinglist', function(req, res, next, id){
+    Shoppinglist.findById(id, function(err, shoppinglist){
+        if (err) {
+            next(err);
+        } else if (!shoppinglist){
+            next(new Error('Failed to load Shoppinglist'));
+        } else {
+            req.shoppinglist = shoppinglist;
+            next();
+        }
+    });
+});
+
+// Shoppinglist Routes  ==============================================
+router.get('/:wg/shoppinglists', function(req, res){
+    req.wg.populate('shoppinglists', function(err, wg){
+        res.json(wg.shoppinglists);
+    });
+});
+
+router.get('/:wg/shoppinglists/:shoppinglist', function(req, res, next){
+   res.json(req.shoppinglist);
+});
+
+router.post('/:wg/shoppinglists', jsonParser, function(req, res, next){
+    var newShoppinglist = new Shoppinglist(req.body);
+    newShoppinglist.wg = req.params.wg;
+    newShoppinglist.save(function(err, newShoppinglist){
+        if (err) return next(err);
+        req.wg.shoppinglists.push(newShoppinglist);
+        req.wg.save(function(err, wg){
+            if (err) return next(err);
+            res.json(newShoppinglist.id);
+        })
+    });
+});
+
+router.delete('/:wg/shoppinglists/:shoppinglist', function(req, res, next){
+    Shoppinglist.remove({"_id": req.params.shoppinglist}, function(err){
+        if (err) next(err);
         res.sendStatus(204);
     });
 });
